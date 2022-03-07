618362793ad7975515e6ed97b3e9ed00e638ade6
Ian McClaflin <ian.mcclaflin@outlook.com>
2022-03-07 06:10:42 -0600

    Foundation work on contact page, not complete yet.

--- PersonalSiteMVC/PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/PersonalSiteMVC/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSiteMVC.Models;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net;
 
 namespace PersonalSiteMVC.Controllers
 {
@@ -34,6 +38,66 @@ namespace PersonalSiteMVC.Controllers
         public ActionResult Resume()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+
+            }
+
+            string message = $"You have received an email from {cvm.Name}.<br />" +
+                $"Subject: {cvm.Subject}<br />" +
+                $"Message: {cvm.Message}<br />" +
+                $"Please respond to {cvm.Email} with your reply.";
+
+            MailMessage mm = new MailMessage(
+
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+
+                cvm.Subject,
+
+                message
+
+                );
+
+            mm.IsBodyHtml = true;
+
+            mm.Priority = MailPriority.High;
+
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+
+            client.Credentials = new NetworkCredential(
+
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+
+                ConfigurationManager.AppSettings["EmailPass"].ToString()
+
+                );
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry, but your request cout not be " +
+                    $"completed at this tie, Please try again later. <br />" +
+                    $"Error Message: {ex.StackTrace}";
+
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+
         }
     }
 }
---
+++ PersonalSiteMVC/PersonalSiteMVC/Models/ContactViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC.Models
+{
+    public class ContactViewModel
+    {
+        //FIELDS - N/A
+
+        //PROPERTIES
+        [Required(ErrorMessage = "*Name is require")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+
+
+    }
+
+
+
+
+
+}
